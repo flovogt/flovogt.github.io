a765ba1e25775affdec891c893e368cbdf1a5536
Florian Vogt <florian.vogt@sap.com>
2024-07-05 16:15:07 +0200

    feat: restructure app (#6)
    * feat: add more content
    
    * feat: add basic routing structure
    
    * feat: maximize look and feel
    
    * feat: project view
    
    * deps: upgrade deps and UI5 version

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,10 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], (Controller) => {
 	"use strict";
-
 	return Controller.extend("flovogt.website.controller.App", {
-
-
+		onInit() {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
 	});
-
 });
